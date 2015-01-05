cb90e8ee6c34ea944d7d0b5fb53597092bb41688
janfrost <janfrost@163.com>
2015-01-06 06:17:45 +0800

    stack

---
+++ 20150105/stack/include/Node.h
@@ -0,0 +1,19 @@
+#ifndef NODE_H
+#define NODE_H
+#include <iostream>
+typedef int Elem;
+
+class Node
+{
+	public:
+	Node(Elem data);
+	Elem getData();
+	Node * getNext();
+	void setData(Elem data);
+	void setNext(Node *node);
+	private:
+	Elem data;
+	Node *next;
+};
+
+#endif // NODE_H
